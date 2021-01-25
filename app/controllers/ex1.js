9bbd1f95088aa9f7e0df11a999a6249acfb0fd00
Tom <21903078@etu.unicaen.fr>
2021-01-25 12:22:40 +0100

    ex1 zone de text ok car restants ok css ok reste à faire

---
+++ app/controllers/ex1.js
@@ -0,0 +1,23 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class Ex1Controller extends Controller {
+  @tracked content = '';
+  @tracked info;
+  MAX = 100;
+
+  get size() {
+    return this.MAX - this.content.length;
+  }
+
+  @action
+  clear() {
+    this.content = '';
+  }
+
+  @action
+  save() {
+    console.log('save');
+  }
+}
