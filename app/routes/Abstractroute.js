5bd86332d87e08d254ab6635a55a652f78d38a17
TomLgpl <74899375+TomLgpl@users.noreply.github.com>
2021-03-25 14:40:55 +0100

    Authentification
    Création d'un service pour l'authentification à l'intranet
    Implémentation de l'action de connexion dans la route index

---
+++ app/routes/Abstractroute.js
@@ -0,0 +1,14 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class AbstractrouteRoute extends Route {
+
+  @service userAuth;
+
+  redirect() {
+    if(!this.userAuth.isAuth) {
+      this.transitionTo('index');
+    }
+  }
+
+}
